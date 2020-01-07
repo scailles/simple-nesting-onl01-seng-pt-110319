0d7a292f4cf10038d75ae010b06335f3a6f2a556
Shaina C <scailles23@gmail.com>
2020-01-07 08:52:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
         }
      }
 
- programmer_hash << {
+ programmer_hash[d]={
      :yukihiro_matsumoto => {
         :known_for => "Ruby",
         :languages => ["LISP", "C"]
